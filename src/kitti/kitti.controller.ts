4fee0488bced42122ee5c0acfa67685da86c0c32
Kittipol <kittipol.surapinchai@gmail.com>
2023-10-12 23:08:31 +0700

    controller

--- src/kitti/kitti.controller.ts
+++ src/kitti/kitti.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Query } from '@nestjs/common';
 
 @Controller('kitti')
-export class KittiController {}
+export class KittiController {
+    @Get()
+    getKitti(@Query('type') type: string) { 
+        return [{ type }];
+    }
+
+    @Get(':id')
+    getOneKitti(@Param('id') id: string) {
+        return {
+            id 
+        };
+    }
+
+    @Post()
+    createKitti() {
+        return {};
+    }
+
+    @Put(':id')
+    updateKitti(@Param('id') id: string) {
+        return {};
+    }
+
+    @Delete(':id')
+    deleteKitti(@Param('id') id: string) {
+        return {};
+    }
+}
